ba1812dc8b60f90d82f61405f73f19d67376f40c
lucasbmiguel <lucas@elemeno.ai>
2023-09-07 21:08:44 -0400

    fix: adjusting params on llama2 to not be set when not user specified

--- semantix_genai_inference/inference/llm/llama2.py
+++ semantix_genai_inference/inference/llm/llama2.py
@@ -9,20 +9,25 @@ class Llama2InferenceClient(SemantixGenClient):
         super().__init__(inference_server_id, api_secret, version=version)
         self._type = "llama2"
 
-    def complete(self, prompt: str, temperature: Optional[float] = 0.1, top_p: Optional[int] = 40, 
-                      top_k: Optional[int] = 80, max_new_tokens: Optional[int] = 1024, 
-                      num_beams: Optional[int] = 4):
+    def complete(self, prompt: str, temperature: Optional[float] = None, top_p: Optional[int] = None, 
+                      top_k: Optional[int] = None, max_new_tokens: Optional[int] = None, 
+                      num_beams: Optional[int] = None):
         return asyncio.run(self._complete_async(prompt, temperature, top_p, top_k, max_new_tokens, num_beams))    
 
     async def _complete_async(self, prompt, temperature, top_p, top_k, max_new_tokens, num_beams):
         body = {
-            "prompt": [prompt],
-            "temperature": temperature,
-            "top_p": top_p,
-            "top_k": top_k,
-            "max_new_tokens": max_new_tokens,
-            "num_beams": num_beams,
+            "prompt": [prompt]
         }
+        if temperature is not None:
+            body["temperature"] = temperature
+        if top_p is not None:
+            body["top_p"] = top_p
+        if top_k is not None:
+            body["top_k"] = top_k
+        if max_new_tokens is not None:
+            body["max_new_tokens"] = max_new_tokens
+        if num_beams is not None:
+            body["num_beams"] = num_beams
         async with aiohttp.ClientSession(headers=self.headers) as session:
             async with session.post(self.url, json=body) as response:
                 return await response.json()
--- semantix_genai_inference/inference/model_client.py
+++ semantix_genai_inference/inference/model_client.py
@@ -3,9 +3,17 @@ from semantix_genai_inference.inference.llm.alpaca import AlpacaInferenceClient
 from semantix_genai_inference.inference.llm.llama2 import Llama2InferenceClient
 
 class ModelClient:
-
+    
     @staticmethod
     def create(client_type, inference_server_id: str, api_secret: Optional[str] = None, version: Optional[str] = "v0"):
+        """ Create a client for a given model type and inference server id
+        
+        Args:
+            client_type (str): The type of model client to create. Valid options are "alpaca" and "llama2"
+            inference_server_id (str): The id of the inference server to connect to
+            api_secret (str, optional): The API secret to use for authentication. Defaults to None.
+            version (str, optional): The version of the model to use. Defaults to "v0".
+        """
         if client_type == "alpaca":
             return AlpacaInferenceClient(inference_server_id, api_secret, version=version)
         elif client_type == "llama2":
